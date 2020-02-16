a261ff22f6c5795c955461c663e4cee1a59a9c31
rsmart6 <rsmart6@ivytech.edu>
2020-02-16 16:11:30 -0600

    listing 17.1 and 17.2

---
+++ class_projects/M05/17.1 and 17.2/upload.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Uploadiong...</title>
+</head>
+<body>
+	<h1>Uploading File...</h1>
+<?php	
+	if ($_FILES['the_file']['error'] > 0)
+	{
+		echo 'Problem: ';
+		switch ($_FILES['the_file']['error'])
+		{
+			case 1:
+				echo 'File exceeded upload_max_filesize.';
+				break;
+			case 2:
+				echo 'File exceeded max_file_size.';
+				break;
+			case 3:
+				echo 'File only partially uploaded.';
+				break;
+			case 4:
+				echo 'No file uploaded.';
+				break;
+			case 6:
+				echo 'Cannot Upload file: No temp directory specified';
+				break;
+			case 7:
+				echo 'Upload failed: Cannot write to disk.';
+				break;
+			case 8:
+				echo 'A PHP extension blocked the file upload.';
+				break;
+		}
+		exit;
+	}
+	//does the file have the right mime type?
+	if ($_FILES['the_file']['type'] != 'image/png')
+	{
+		echo 'Problem: file is not a PNG image.';
+		exit;
+	}
+	//put the file where we'd like it
+	$uploaded_file = '/xampp/htdocs/scripts/17.1 and 17.2/'.$_FILES['the_file']['name'];
+	if (is_uploaded_file($_FILES['the_file']['tmp_name']))
+	{
+	if (!move_uploaded_file($_FILES['the_file']['tmp_name'], $uploaded_file))
+	{echo 'Problem: Could not move file to destionation directory.';
+	exit;
+	}
+	}
+	else
+	{
+		echo 'Problem: Possible file upload attack. Filename:';
+		echo $_FILES['the_file']['name'];
+		exit;
+	}
+	echo 'File uploaded successfully.';
+	
+	//show what was uploaded
+	echo '<p>You uploaded the following image:<br/>';
+	echo '<img src="'.$_FILES['the_file']['name'].'"/>';
+	?>
+	</body>
+	</html>
+	
+	
+		
